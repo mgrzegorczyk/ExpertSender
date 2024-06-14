423702c51709f22b9e3b75aa14eebec7988c0828
mgrzegor <m-grzegorczyk@o2.pl>
2024-06-14 15:42:11 +0200

    person details validation attributes

--- src/ExpertSender.Application/Models/PersonDetails.cs
+++ src/ExpertSender.Application/Models/PersonDetails.cs
@@ -1,9 +1,15 @@
-﻿namespace ExpertSender.Application.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpertSender.Application.Models;
 
 public class PersonDetails
 {
     public int Id { get; set; }
+    [Required]
+    [MinLength(3, ErrorMessage = "First Name must be at least 3 characters long.")]
     public string FirstName { get; set; }
+    [Required]
+    [MinLength(3, ErrorMessage = "Last Name must be at least 3 characters long.")]
     public string LastName { get; set; }
     public string Description { get; set; }
     public List<EmailDetails> Emails { get; set; } = new List<EmailDetails>();
