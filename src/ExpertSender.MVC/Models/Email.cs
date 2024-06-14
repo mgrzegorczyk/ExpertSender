3561c0f60a92ec89d3d4090a7b7b9d50834ddc1a
mgrzegor <m-grzegorczyk@o2.pl>
2024-06-14 07:01:11 +0200

    initial - mvc app with models

---
+++ src/ExpertSender.MVC/Models/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpertSender.MVC.Models;
+
+public class Email
+{
+    public int Id { get; set; }
+    public string EmailAddress { get; set; }
+    public int PersonId { get; set; }
+    public Person Person { get; set; }
+}
---
+++ src/ExpertSender.MVC/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpertSender.MVC.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Description { get; set; }
+    public ICollection<Email> Emails { get; set; }
+}
